4c6636bee8cb94e67de6db932d190d3118cb0fe0
Nick Earl <nick.r.earl@gmail.com>
2022-08-21 22:22:39 -0400

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, } from "react-router-dom"
 import './style.css';
 import Header from './components/Header'
 import AboutPage from './pages/About'
